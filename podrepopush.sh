649d3ad91025e5fd24d399ec33319ba4702882b5
zhuxuhong <1906457616@qq.com>
2020-01-10 10:14:37 +0800

    更新TestPod

--- podrepopush.sh
+++ podrepopush.sh
@@ -8,25 +8,26 @@
 
 
 
-echo ""
-echo "=======   0.检查ZSSpecs目录    ======="
-
-echo ""
+echo "|------------------------------------|"
+echo "|         0.检查PodSpecs目录           |"
+echo "|------------------------------------"
 
 current_Folder=$(pwd)
 
+remote_specs="git@github.com:BackWorld/PodSpecs.git"
 
-SpecsFolder="/Users/"$USER"/ZSSpecs"
+SpecsFolder="/Users/"$USER"/PodSpecs"
 
 echo "SpecsFolder="$SpecsFolder
 
-echo "=======    检查ZSSpecs目录存不存在"
+echo "检查PodSpecs目录存不存在"
 
 if [ ! -d "$SpecsFolder" ]; then
-echo "=======    如果不存在就创建父级目录 然后git clone  ======="
-echo "cd /Users/"$USER
+echo "PodSpecs目录不存在"
+echo "[  cd /Users/"$USER "  ]"
 cd "/Users/"$USER
-git clone git@gitee.com:flytoo/ZSSpecs.git
+echo "[  git clone"$remote_specs "  ]"
+git clone $remote_specs
 
 if [ $? -ne 0 ]
 then
@@ -35,11 +36,11 @@ exit 1
 fi
 
 else
-echo "ZSSpecs已经存在"
+echo "PodSpecs已经存在"
 echo "目录"$SpecsFolder
 cd $SpecsFolder
-echo "cd "$SpecsFolder
-echo "同步ZSSpecs"
+echo "[  cd "$SpecsFolder"  ]"
+echo "同步PodSpecs"
 git pull origin master
 
 if [ $? -ne 0 ]
@@ -53,11 +54,13 @@ fi
 
 cd $current_Folder
 
-echo ""
-echo "=======    1.获取podspec文件名称  ======="
+echo "|------------------------------------|"
+echo "|                                    |"
+echo "|        1.获取podspec文件名称         |"
+echo "|                                    |"
+echo "|------------------------------------"
 
-echo ""
-echo "cd "$current_Folder
+echo "[  cd "$current_Folder"  ]"
 
 #获取podspec文件名称
 podspec_fileName=$(find *.podspec)
@@ -65,33 +68,33 @@ echo "podspec_fileName = "$podspec_fileName
 libName=`echo $podspec_fileName | cut -d \. -f 1`
 
 
-echo ""
-echo "=======    2.更新lib  ======="
+echo "|------------------------------------|"
+echo "|                                    |"
+echo "|               2.更新lib             |"
+echo "|                                    |"
+echo "|------------------------------------"
 
-echo ""
-echo "=======    更新lib  ======="
-echo "=======    git status  ======="
+echo "[  git status  ]"
 git status
-echo "=======    git add .   ======="
+echo "[  git add .   ]"
 git add .
-echo "=======    git commit  ======="
+echo "[  git commit  ]"
 git commit -m '更新'$libName
-echo "=======    git pull    ======="
+echo "[  git pull    ]"
 git pull origin master
-echo "=======    git push    ======="
+echo "[  git push    ]"
 git push origin master
-
-
-
 
 
 #获取当前的版本号
 last_version=$(cat $podspec_fileName | grep -E -o "([0-9]{1,3}[\.]){2}[0-9]{1,3}"|head -n 1)
 
-echo ""
-echo "=======    3.增加版本号  ======="
+echo "|------------------------------------|"
+echo "|                                    |"
+echo "|             3.增加版本号             |"
+echo "|                                    |"
+echo "|------------------------------------"
 
-echo ""
 echo "last_version = "$last_version
 #分割当前的版本号
 v1=`echo $last_version | cut -d \. -f 1`
@@ -117,11 +120,11 @@ new_version=$v1"."$v2"."$v3
 echo "new_version = "$new_version
 #替换版本号
 
-echo ""
-echo "=======    4.替换版本号，更新lib    ======="
-
-echo ""
-echo "=======    替换版本号  ======="
+echo "|------------------------------------|"
+echo "|                                    |"
+echo "|     4.替换版本号，更新lib             |"
+echo "|                                    |"
+echo "|------------------------------------"
 
 sed -i "" "/.version/s/$last_version/$new_version/" "$podspec_fileName"
 
@@ -131,18 +134,15 @@ echo "失败！"
 exit 1
 fi
 
-echo ""
-echo ""
-echo ""
-echo "=======    更新lib  ======="
+echo "更新lib"
 
-echo "=======    git status  ======="
+echo "[  git status  ]"
 git status
-echo "=======    git add .  ======="
+echo "[  git add .  ]"
 git add .
-echo "=======    git commit  ======="
+echo "[  git commit  ]"
 git commit -m '更新版本到'$new_version
-echo "=======    git pull  ======="
+echo "[  git pull  ]"
 git pull origin master
 
 if [ $? -ne 0 ]
@@ -151,15 +151,18 @@ echo "失败！"
 exit 1
 fi
 
-echo "=======    git push  ======="
+echo "[  git push  ]"
 git push origin master
 
-echo ""
-echo "=======    5.添加tag  ======="
+echo "|------------------------------------|"
+echo "|                                    |"
+echo "|             5.添加tag               |"
+echo "|                                    |"
+echo "|------------------------------------"
 
 echo ""
 #绑定tag
-echo "=======    git tag  ======="
+echo "[  git tag  ]"
 git tag "$new_version"
 
 if [ $? -ne 0 ]
@@ -168,7 +171,7 @@ echo "失败！"
 exit 1
 fi
 
-echo "=======    git push --tags  ======="
+echo "[  git push --tags  ]"
 git push --tags
 
 if [ $? -ne 0 ]
@@ -178,12 +181,12 @@ exit 1
 fi
 
 
+echo "|------------------------------------|"
+echo "|                                    |"
+echo "|     6.拷贝.podspec到PodSpecs工程      |"
+echo "|                                    |"
+echo "|------------------------------------"
 
-
-echo ""
-echo "=======    6.拷贝.podspec到ZSSpecs工程  ======="
-
-echo ""
 new_version_Folder=$SpecsFolder"/"$libName"/"$new_version
 echo "new_version_Folder = "$new_version_Folder
 
@@ -191,7 +194,7 @@ libName_Folder=$SpecsFolder"/"$libName
 echo "libName_Folder = "$libName_Folder
 
 if [ ! -d "$libName_Folder" ]; then
-echo "=======    如果不存在就创建父级目录======="
+echo $libName_Folder"不存在"
 echo "创建"$libName_Folder
 mkdir $libName_Folder
 if [ $? -ne 0 ]
@@ -202,34 +205,31 @@ fi
 fi
 
 cd $libName_Folder
-echo "=======    cd  ======="$libName_Folder
+echo "[  cd  ]"$libName_Folder
 mkdir $new_version
-echo "=======    mkdir  ======="$new_version
+echo "[  mkdir  ]"$new_version
 
 cd $current_Folder
-echo "=======    拷贝"$podspec_fileName"到新版本目录  ======="$new_version_Folder"/"$podspec_fileName
+echo "[  cp "$podspec_fileName $new_version_Folder"/"$podspec_fileName"  ]"
 cp $podspec_fileName $new_version_Folder"/"$podspec_fileName
 
 
+echo "|------------------------------------|"
+echo "|                                    |"
+echo "|             7.更新PodSpecs           |"
+echo "|                                    |"
+echo "|------------------------------------"
 
-echo ""
-echo "=======    7.更新ZSSpecs  ======="
-
-echo ""
 cd $SpecsFolder
 echo "cd "$SpecsFolder
 
-echo ""
-echo ""
-echo ""
-echo "=======    更新ZSSpecs  ======="
-echo "=======    git status  ======="
+echo "[  git status  ]"
 git status
-echo "=======    git add .  ======="
+echo "[  git add .  ]"
 git add .
-echo "=======    git commit  ======="
+echo "[  git commit  ]"
 git commit -m "[Update] $libName ($new_version)"
-echo "=======    git pull  ======="
+echo "[  git pull  ]"
 git pull origin master
 
 if [ $? -ne 0 ]
@@ -238,13 +238,14 @@ echo "失败！"
 exit 1
 fi
 
-echo "=======    git push  ======="
+echo "[  git push  ]"
 git push origin master
 
-echo ""
-echo ""
-echo ""
-echo "========  发布成功！  new_version = "$new_version"  ========"
-echo ""
-echo ""
-echo ""
+
+echo "|------------------------------------|"
+echo "|                                    |"
+echo "|             发布成功！               |"
+echo "|            "$new_version"          |"
+echo "|                                    |"
+echo "|------------------------------------"
+
